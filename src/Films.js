8adcd1d07805b336261ef9cbede746938eff7eef
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-31 14:45:32 -0500

    adds files for intro to classes in react

---
+++ src/Films.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MovieList() {
+    return (
+        <ul>
+            <li>Devil Wears Prada</li>
+            <li>A Cry in the Dark</li>
+            <li>Sophie's Choice</li>
+
+        </ul>
+    )
+};
+
+export default MovieList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import { throwStatement } from "@babel/types";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// class Hero{
+//     constructor(health=18, power=5){
+//         this.health = health;
+//         this.power = power;
+//     }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//     hug(otherHero) {
+//         otherHero.health += this.power;
+//         console.log(`otherHero has ${otherHero.health}`);
+//     }
+//     recieveHealth(healthPoints){
+//         this.health += healthPoints;
+//         console.log(`Yay! I lovae ${this.health}`);
+
+//     }
+
+
+// }
+
+// const aHero = new Hero();
+// const anotherHero = new Hero();
+// aHero.hug(anotherHero);
+// aHero.recieveHealth(anotherHero);
+
+// console.log(aHero);
+
+
+// class Cat {
+//     constructor (breed, age) {
+//         this.breed = breed;
+//         this.age = age;
+//     }
+//     eatFood(amountOfFood){
+//        this.age -= amountOfFood
+//     }
+//     hasSleep(amountOfSleep){
+//         this.age -= amountOfSleep
+//     }
+// };
+// const aCat = new Cat('yellow', '11')
+// aCat.eatFood(3);
+// aCat.hasSleep(2);
+// // aCat.studyMore(7);
+// console.log(aCat)
+
+// class Student {
+//     constructor (studyTime, grade){
+//         this.studyTime= studyTime;
+//         this.grade = grade;
+//     }
+//     studyMore (moreTimeSpent){
+//         this.grade += moreTimeSpent;
+//     }
+//     studyLess (lessTimeSpent){
+//         this.grade -= lessTimeSpent
+//     }
+// };
+
+// const aStudent = new Student(2,3.0);
+// const twoStudent = new Student (3, 4.0);
+
+// twoStudent.studyLess(1);
+// aStudent.studyMore(5);
+
+// console.log(twoStudent);
+// console.log(aStudent);
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
