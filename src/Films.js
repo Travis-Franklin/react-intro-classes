d5bcb0361c20403f13a5f59a44f83a23a918d764
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-31 16:04:10 -0500

    created class with custom click handlers that modifies state

--- src/Films.js
+++ src/Films.js
@@ -1,14 +1,45 @@
 import React from 'react';
 
-function MovieList() {
-    return (
-        <ul>
-            <li>Devil Wears Prada</li>
-            <li>A Cry in the Dark</li>
-            <li>Sophie's Choice</li>
 
-        </ul>
-    )
-};
+function _handleMouseOver () {
+    console.log('ouch');
+}
+
+// const movies = [
+    //     'Devil Wears Prada',
+    //     'A Cry in the Dark',
+    //     'Sophies Choice'
+    // ]
+    
+    class MovieList extends React.Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                movies: [
+                    'Devil Wears Prada',
+                    'A Cry in the Dark',
+                    'Sophies Choice',
+                    'Mama Mia'
+                ]
+            }
+        }
+        render () {
+            return (
+                <ul>
+                {this.state.movies.map((movie, i ) => <li onClick={ () => this._handleClick(i)}>{movie}</li>)}
+            </ul>
+        )
+    }
+        _handleClick = (index) => {
+            const newMovies = [
+                ...this.state.movies];
+                newMovies[index] += '!';
+                this.setState({
+                    movies: newMovies
+                });
+        }
+
+}
+
 
 export default MovieList;
